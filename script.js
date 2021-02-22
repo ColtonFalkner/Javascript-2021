a347f501d5e530d5f06f8c2c386169f326b11630
Colton <coltonfalkner@gmail.com>
2021-02-22 16:36:37 -0500

    finally something to commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let country = 'United States of America'
+let continent = 'North America'
+let population = 328000000
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+let isIsland = false
+let language
+
+console.log(typeof country)
+console.log(typeof continent)
+console.log(typeof population)
+console.log(typeof isIsland)
+console.log(typeof language)
