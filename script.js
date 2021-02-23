7864638b1e0bc13f08879f72f44cb49199cb2252
Colton <coltonfalkner@gmail.com>
2021-02-23 15:55:29 -0500

    another challenge down

--- script.js
+++ script.js
@@ -7,10 +7,24 @@ console.log(continent)
 console.log(population)
 
 const isIsland = false
-let language
+let language = 'English'
 
-console.log(typeof country)
-console.log(typeof continent)
-console.log(typeof population)
-console.log(typeof isIsland)
-console.log(typeof language)
+// console.log(typeof country)
+// console.log(typeof continent)
+// console.log(typeof population)
+// console.log(typeof isIsland)
+// console.log(typeof language)
+
+//  population = population / 2
+// population++
+
+// console.log(population >= 33000000)
+
+// description = `${country} is in ${continent}, and it's ${population} speak ${language}`
+// console.log(description)
+
+if (population >= 33000000) {
+  console.log("America's population is above average")
+} else {
+  console.log("America's population is below average")
+}
