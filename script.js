e5950435d8949341c04b7b779f6c3e19defd7046
Colton <coltonfalkner@gmail.com>
2021-03-03 11:31:37 -0500

    these challenges are fun

--- script.js
+++ script.js
@@ -23,8 +23,46 @@ let language = 'English'
 // description = `${country} is in ${continent}, and it's ${population} speak ${language}`
 // console.log(description)
 
-if (population >= 33000000) {
-  console.log("America's population is above average")
-} else {
-  console.log("America's population is below average")
+// if (population >= 33000000) {
+//   console.log("America's population is above average")
+// } else {
+//   console.log("America's population is below average")
+// }
+
+// console.log('9' - '5')
+
+// console.log('19' - '13' + '17')
+// console.log('19' - '13' + 17)
+// console.log('123' < 57)
+// console.log(5 + 6 + '4' + 9 - 4 - 2)
+
+// const numNeighbors = prompt('How many neighbors does your country have?')
+// if (numNeighbors == 1) {
+//   console.log('Only one border')
+// } else if (numNeighbors == 2) {
+//   console.log('You got them two neighbors over there do ya?')
+// } else {
+//   console.log('There are no neighbors. What is this an island?')
+// }
+
+switch (language) {
+  case 'Chinese':
+  case 'Mandarin':
+    console.log('Most Number of native Speakers')
+    break
+  case 'Spanish':
+    console.log('Second most spoken language')
+    break
+  case 'English':
+    console.log('third place bababy')
+    break
+  case 'Hindi':
+    console.log('fourth placed language')
+    break
+  case 'Arabic':
+    console.log('Fifth place, but on the rise.')
+    break
+
+  default:
+    console.log('Not bad either!!')
 }
